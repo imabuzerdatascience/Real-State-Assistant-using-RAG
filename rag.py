db385c3fbe1b5cfd9a7979a15c19c1fd312ab8cb
Mohammad Abuzer Khan <abuzerdatascience@gmail.com>
2026-07-12 17:52:57 +0545

    update bew things in projects

--- rag.py
+++ rag.py
@@ -6,28 +6,33 @@ from langchain_community.document_loaders import UnstructuredURLLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter 
 from langchain_chroma import Chroma
 from langchain_huggingface import HuggingFaceEmbeddings
-from langchain_classic.chains import RetrievalQAWithSourcesChain
+from langchain_classic.chains import RetrievalQAWithSourcesChain 
+import os 
 
 
-print("intilized components")
-load_dotenv()  
+
+load_dotenv()
+
+
 # avoid dupiction 
 llm = None 
 vector_store = None
 
 def intialize_components():
   global llm , vector_store 
-  if llm is None :
-     llm = ChatGroq(model="llama-3.3-70b-versatile" , temperature=0.8  , max_tokens = 500) 
 
-  ef = HuggingFaceEmbeddings(
-    model_name =  "sentence-transformers/all-MiniLM-L6-v2" ,
-    model_kwargs = {"trust_remote_code": True}
-  )
+  if llm is None :
+     llm = ChatGroq(groq_api_key=os.getenv("GROQ_API_KEY") , model="llama-3.3-70b-versatile" , temperature=0.8  , max_tokens = 500) 
+
+  if vector_store is None :
+    ef = HuggingFaceEmbeddings(
+      model_name =  "sentence-transformers/all-MiniLM-L6-v2" ,
+      model_kwargs = {"trust_remote_code": True}
+    )
 
 
 # vector _db 
-  if vector_store is None : 
+ 
     vector_store = Chroma(
       collection_name = "real_state" ,
       embedding_function= ef ,
@@ -35,28 +40,51 @@ def intialize_components():
     )
 
 
-def process_url(urls) :
+def process_url(urls):
+
   
-  loader = UnstructuredURLLoader(urls=urls)
-  data = loader.load() 
+    yield "Initializing LLM and Vector Database..."
+
+    intialize_components()
+    vector_store.reset_collection()
+
+    yield "Loading URLs..."
+
+    loader = UnstructuredURLLoader(urls=urls)
+    data = loader.load()
+
+    print("Documents:", len(data))
+
+    for doc in data:
+      print("=" * 50)
+      print(doc.metadata)
+      print(doc.page_content[:1000])
+
+    text_splitter = RecursiveCharacterTextSplitter(
+        chunk_size=1000,
+        separators=["\n\n", "\n", ".", ",", "?", "&"]
+    )
+
+    yield "Splitting documents into chunks..."
+
+    docs = text_splitter.split_documents(data)
+
+    yield f"Creating embeddings for {len(docs)} chunks..."
+
+    uuids = [str(uuid4()) for _ in range(len(docs))]
+    vector_store.add_documents(docs, ids=uuids)
+
+    yield "Embeddings stored in Vector Database successfully."
+
+
+def genrate_answer(query):
+  if not vector_store :
+    raise ValueError("you database is not intialize")
   
-
-  intialize_components()
-  vector_store.reset_collection()
-
-  # chunkers 
-  text_splitter = RecursiveCharacterTextSplitter(
-    chunk_size=100,
-    separators=["\n\n", "\n", ".", ",", "?", "&"],
-    chunk_overlap=50
-)
-
-  docs = text_splitter.split_documents(data)
-
-  uuids = [str(uuid4()) for _ in range(len(docs))] 
-  vector_store.add_documents(docs , ids = uuids)
-
-
+  chain = RetrievalQAWithSourcesChain.from_llm(llm=llm ,retriever = vector_store.as_retriever())
+  result = chain.invoke({"question" : query} , return_only_outputs = True)
+  sources = result.get("sources" , "")
+  return result["answer"] , sources 
 
 
 
@@ -64,16 +92,18 @@ def process_url(urls) :
 
 if __name__ == "__main__" :
   urls = [
-          "https://realty.economictimes.indiatimes.com/news/residential/former-ask-asset-directors-family-invests-179-crore-in-luxury-mumbai-apartments/132301523?utm_source=top_story&utm_medium=homepage" ,
-  ]
+        "https://www.cnbc.com/2024/12/21/how-the-federal-reserves-rate-policy-affects-mortgages.html",
+        "https://www.cnbc.com/2024/12/20/why-mortgage-rates-jumped-despite-fed-interest-rate-cut.html"
+    ]
+
    
-  process_url(urls)
+  for status in process_url(urls):
+    print(status)
 
   
-  results = vector_store.similarity_search(
-    query="Family buys apartment",
-    k=2
-    
-) 
-print(results)
-  
+  answer , sources = genrate_answer("Tell me what was the 30 year fixed mortagate rate along with the date?")
+  print(f"answer : {answer}")
+  print(f"sources : {sources}") 
+
+
+
